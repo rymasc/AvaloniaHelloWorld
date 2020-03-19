5dfa522676336f252aa72a1b122eeb91e07fc471
rymasc <ryan.matt.scott@gmail.com>
2020-03-19 17:19:01 -0400

    Create Simple UI and Hello Model

--- AvaloniaHelloWorld/MainWindow.xaml.cs
+++ AvaloniaHelloWorld/MainWindow.xaml.cs
@@ -1,21 +1,19 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using AvaloniaHelloWorld.Models;
 
-namespace AvaloniaHelloWorld
-{
-    public class MainWindow : Window
-    {
-        public MainWindow()
-        {
+namespace AvaloniaHelloWorld{
+    public class MainWindow : Window{
+        public MainWindow(){
             InitializeComponent();
+            this.DataContext = new HelloViewModel() { Greeting = "Enter a Name and click the button" };
 #if DEBUG
             this.AttachDevTools();
 #endif
         }
 
-        private void InitializeComponent()
-        {
+        private void InitializeComponent(){
             AvaloniaXamlLoader.Load(this);
         }
     }
---
+++ AvaloniaHelloWorld/Models/HelloViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace AvaloniaHelloWorld.Models{
+    class HelloViewModel : INotifyPropertyChanged{
+
+        private string _greeting;
+        private string _name;
+
+        public string Greeting {
+            get => _greeting;
+            set {
+                if (value != _greeting) {
+                    _greeting = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Name {
+            get => _name;
+            set {
+                if (value != _name) {
+                    _name = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null){
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
